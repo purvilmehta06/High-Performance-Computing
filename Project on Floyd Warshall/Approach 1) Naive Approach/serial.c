3d3955db974b3ee91f66404f6a7b1cd77eaea67a
Purvil Mehta <purvilmehta06@gmail.com>
2020-05-03 10:02:03 +0530

    Add Project Files

---
+++ Project on Floyd Warshall/Approach 1) Naive Approach/serial.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+#include<math.h>
+#include<omp.h>
+#include<time.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+//  Using the MONOTONIC clock 
+#define CLK CLOCK_MONOTONIC
+
+/* Function to compute the difference between two points in time */
+struct timespec diff(struct timespec start, struct timespec end);
+
+/* 
+   Function to computes the difference between two time instances
+
+   Taken from - http://www.guyrutenberg.com/2007/09/22/profiling-code-using-clock_gettime/ 
+
+   Further reading:
+http://stackoverflow.com/questions/6749621/how-to-create-a-high-resolution-timer-in-linux-to-measure-program-performance
+http://stackoverflow.com/questions/3523442/difference-between-clock-realtime-and-clock-monotonic
+ */
+struct timespec diff(struct timespec start, struct timespec end){
+	struct timespec temp;
+	if((end.tv_nsec-start.tv_nsec)<0){
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	}
+	else{
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
+
+int main(int argc, char* argv[])
+{
+	struct timespec start_e2e, end_e2e, start_alg, end_alg, e2e, alg;
+	/* Should start before anything else */
+	clock_gettime(CLK, &start_e2e);
+
+	/* Check if enough command-line arguments are taken in. */
+	if(argc < 3){
+		printf( "Usage: %s n p \n", argv[0] );
+		return -1;
+	}
+
+	int N=atoi(argv[1]);	/* size of input array */
+	int P=atoi(argv[2]);	/* number of processors*/
+	char *problem_name = "matrix_multiplication";
+	char *approach_name = "block";
+//	char buffer[10];
+//	FILE* inputFile;
+	FILE* outputFile;
+	//	inputFile = fopen(argv[3],"r");
+
+	char outputFileName[50];		
+	sprintf(outputFileName,"output/%s_%s_%s_%s_output.txt",problem_name,approach_name,argv[1],argv[2]);
+
+	//***************
+	int i, j, k;
+
+	// allocate memory for the required arrays
+	int (*adj_mat)[N] = malloc(N * sizeof(*adj_mat));
+	int (*dis)[N] = malloc(N * sizeof(*dis));
+
+	// make the graph valid, edge i->j = edge j->i
+	for(i=0; i<N; i++) {
+		for(j=i+1; j<N; j++){
+			adj_mat[i][j] = adj_mat[j][i];
+		}
+	}
+
+	// initialise the dis matrix, no intermediate nodes
+	// edges themselves are the optimum path
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			if(i == j){
+				dis[i][j] = 0;
+			} else {
+				dis[i][j] = adj_mat[i][j];
+			}
+		}
+	}
+
+
+	clock_gettime(CLK, &start_alg);	/* Start the algo timer */
+
+	/*----------------------Core algorithm starts here----------------------------------------------*/
+
+	for(k=0; k<N; k++) {
+		for(i=0; i<N; i++) {
+			// as discussed in the report
+			// this row will not be updated so we can skip it
+			if(i == k) continue;
+
+			for(j=0; j<N; j++) {
+				if(dis[i][j] > dis[i][k]+dis[k][j]) {
+					dis[i][j] = dis[i][k]+dis[k][j];
+				}
+			}
+		}	
+	}
+
+	/*----------------------Core algorithm finished--------------------------------------------------*/
+
+	clock_gettime(CLK, &end_alg);	/* End the algo timer */
+	/* Ensure that only the algorithm is present between these two
+	   timers. Further, the whole algorithm should be present. */
+
+
+	/* Should end before anything else (printing comes later) */
+	clock_gettime(CLK, &end_e2e);
+	e2e = diff(start_e2e, end_e2e);
+	alg = diff(start_alg, end_alg);
+
+	/* problem_name,approach_name,n,p,e2e_sec,e2e_nsec,alg_sec,alg_nsec
+	   Change problem_name to whatever problem you've been assigned
+	   Change approach_name to whatever approach has been assigned
+	   p should be 0 for serial codes!! 
+	 */
+	printf("%s,%s,%d,%d,%d,%ld,%d,%ld\n", problem_name, approach_name, N, P, e2e.tv_sec, e2e.tv_nsec, alg.tv_sec, alg.tv_nsec);
+
+	return 0;
+
+}
+
